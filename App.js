bc830b7336382e6f4306a5a4729e61b2472f66af
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-12-05 15:04:35 +0700

    Add function create new account
    and veritifiation email sent
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- App.js
+++ App.js
@@ -7,10 +7,10 @@
  * @author hungphan2001
  */
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React, { useState, useEffect } from 'react';
-import { firebase } from './config';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React, {useState, useEffect} from 'react';
+import {firebase} from './config';
 import Login from './src/Login';
 import Registration from './src/Registration';
 import Dashboard from './src/Dashboard';
@@ -25,8 +25,9 @@ function App() {
   //Handle user state changes
   function onAuthStateChanged(user) {
     setUser(user);
-    if (initializing) setInitializing(false);
-
+    if (initializing) {
+      setInitializing(false);
+    }
   }
 
   useEffect(() => {
@@ -34,15 +35,17 @@ function App() {
     return subscriber;
   }, []);
 
-  if (initializing) return null;
+  if (initializing) {
+    return null;
+  }
   if (!user) {
     return (
       <Stack.Navigator>
         <Stack.Screen
-          name="Login" component={Login}
+          name="Login"
+          component={Login}
           options={{
-            headerTitle: () =>
-              <Header name="Bug" />,
+            headerTitle: () => <Header name="Bug" />,
             headerStyle: {
               height: 150,
               borderBottomLeftRadius: 50,
@@ -50,24 +53,24 @@ function App() {
               backgroundColor: '#00e4d0',
               shadowColor: '#000',
               elevation: 25,
-            }
-          }} />
+            },
+          }}
+        />
         <Stack.Screen
           name="Registration"
           component={Registration}
           options={{
-            headerTitle: () =>
-              <Header name="Bug"></Header>,
+            headerTitle: () => <Header name="Bug" />,
             headerStyle: {
               height: 150,
               borderBottomLeftRadius: 50,
               borderBottomRightRadius: 50,
               backgroundColor: '#00e4d0',
               shadowColor: '#000',
-              elevation: 25
-            }
-          }}>
-        </Stack.Screen>
+              elevation: 25,
+            },
+          }}
+        />
       </Stack.Navigator>
     );
   }
@@ -78,8 +81,7 @@ function App() {
         name="Dashboard"
         component={Dashboard}
         options={{
-          headerTitle: () =>
-            <Header name="Dashboard"></Header>,
+          headerTitle: () => <Header name="Dashboard" />,
           headerStyle: {
             height: 150,
             borderBottomLeftRadius: 50,
@@ -87,9 +89,9 @@ function App() {
             backgroundColor: '#00e4d0',
             shadowColor: '#000',
             elevation: 25,
-          }
-        }}>
-      </Stack.Screen>
+          },
+        }}
+      />
     </Stack.Navigator>
   );
 }
@@ -97,7 +99,7 @@ function App() {
 export default () => {
   return (
     <NavigationContainer>
-      <App></App>
+      <App />
     </NavigationContainer>
-  )
-}
+  );
+};
--- src/Registration.js
+++ src/Registration.js
@@ -1,12 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,TextInput,TouchableOpacity,StyleSheet } from 'react-native';
+import { useState } from 'react';
+import {firebase } from '../config';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+
 
 const Registration = () => {
-  return (
-    <View>
-      <Text>Registration</Text>
+  const navigation = useNavigation();
+  const [email,setEmail] = useState();
+  const [password,setPassword] = useState();
+  const [firstName,setFirstName] = useState();
+  const [lastName,setLastName] = useState();
+
+  registrationUser = async(email,password,lastName,firstName) =>{
+    await firebase.auth().createUserWithEmailAndPassword(email,password)
+    .then(()=>{
+      firebase.auth().currentUser.sendEmailVerification({
+        handleCodeInApp:true,
+        url:'test-auth-edcfd.firebaseapp.com',
+      })
+      .then(() =>{
+        alert('Veritifiation email sent');
+      }).catch((error) =>{
+        alert(error.message)
+      })
+      .then(()=>{
+        firebase.firestore().collection('user')
+        .doc(firebase.auth().currentUser.uid)
+        .set({
+          firstName,
+          lastName,
+          email,
+        })
+      })
+      .catch((error)=>{
+        alert(error.message)
+      })
+    })
+    .catch((error)=>{
+      alert(error.message)
+    })
+  }
+  return(
+    <View style={styles.container}>
+      <Text style={}></Text>
     </View>
   )
 }
 
 export default Registration
+
+const styles =StyleSheet.create({
+  container :{
+    flex :1,
+    alignItems:'center',
+    margintop:100,
+  }
+})
